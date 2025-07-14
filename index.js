8cfe9f0f7260141ec35bd562006e21d0a231bbeb
bbenhu <bbenhu@gmail.com>
2025-07-14 08:28:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const PORT = process.env.PORT || 10000;
 app.get('/isLive/:username', (req, res) => {
     const username = req.params.username;
     const connection = new TikTokLiveConnection(username);
-
-    connection.connect().then(state => {
+    console.log(connection.fetchIsLive());
+    res.json({ username, live: connection.fetchIsLive() });
+    
+    /*connection.connect().then(state => {
         console.log(`✅ ${username} élőben van! RoomID: ${state.roomId}`);
         connection.disconnect(); // lezárjuk a kapcsolatot
         res.json({ username, live: true, state: state });
     }).catch(err => {
         console.warn(`❌ ${username} nem élőzik vagy nem elérhető: ${err.message}`);
         res.json({ username, live: false, error: err.message });
-    });
+    });*/
 });
 
 app.get('/', (req, res) => {
