49914395a3e81a69f24d0bfd721b2e15e0b44b7f
bbenhu <bbenhu@gmail.com>
2025-07-14 07:23:42 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { WebcastPushConnection } = require('tiktok-live-connector');
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get('/isLive/:username', async (req, res) => {
+    const username = req.params.username;
+    const connection = new WebcastPushConnection(username);
+    try {
+        await connection.connect();
+        await connection.disconnect();
+        res.json({ username, live: true });
+    } catch (e) {
+        res.json({ username, live: false });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
